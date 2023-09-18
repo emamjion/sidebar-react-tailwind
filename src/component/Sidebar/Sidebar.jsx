024ed9517ec6f2b1c7a08df50a952633f57e07c6
emamjion <emamjion@gmail.com>
2023-09-18 06:30:28 +0600

    I finished this work

--- src/component/Sidebar/Sidebar.jsx
+++ src/component/Sidebar/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { HiMenuAlt3 } from "react-icons/hi";
 import { MdOutlineDashboard } from "react-icons/md";
 import { AiOutlineUser } from "react-icons/ai";
@@ -30,7 +30,8 @@ const Sidebar = () => {
         {
             name: 'analytics',
             link: '/',
-            icon: TbReportAnalytics
+            icon: TbReportAnalytics,
+            margin: true
         },
         {
             name: 'File Manager',
@@ -45,7 +46,8 @@ const Sidebar = () => {
         {
             name: 'Saved',
             link: '/',
-            icon: AiOutlineHeart
+            icon: AiOutlineHeart,
+            margin: true
         },
         {
             name: 'Setting',
@@ -53,24 +55,29 @@ const Sidebar = () => {
             icon: RiSettings4Line
         }
     ]
-    console.log(menus)
+    const [open, setOpen] = useState(true);
     
     return (
         <section className='flex gap-6'>
-            <div className='bg-[#2c3e50] min-h-screen w-72 text-gray-100 px-4'>
+            <div className={`bg-[#2c3e50] min-h-screen ${open ? 'w-72' : 'w-16'} text-gray-100 px-4 duration-500`}>
                 <div className='py-3 flex justify-end'>
-                    < HiMenuAlt3 size={26} className='cursor-pointer' />
+                    < HiMenuAlt3 
+                        size={26} 
+                        className='cursor-pointer'
+                        onClick={() => setOpen(!open)}
+                    />
                 </div>
                 <div className='mt-4 flex flex-col gap-4 relative'>
                     {
                         menus.map((menu, i) => <Link
                             key={i}
-                            className='flex items-center gap-4'
+                            className={` ${menu?.margin && 'mt-5'} group flex items-center gap-3.5 font-medium p-2 hover:bg-gray-800 rounded-md`}
                         >
                             <div>
                                 {React.createElement(menu?.icon, {size: '20'})}
                             </div>
-                            <h2>{menu?.name}</h2>
+                            <h2 className={`whitespace-pre duration-500 ${!open && 'opacity-0 translate-x-28 overflow-hidden'}`} style={{transitionDelay: `${i + 3}00ms`}}>{menu?.name}</h2>
+                            <h2 className={` ${open && 'hidden'} absolute left-48 bg-white font-semibold whitespace-pre text-gray-900 rounded-md drop-shadow-lg px-0 py-0 w-0 overflow-hidden group-hover:px-2 group-hover:py-1 group-hover:left-14 group-hover:duration-300 group-hover:w-fit`}>{menu?.name}</h2>
                         </Link>)
                     }
                     
